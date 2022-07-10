8eec56ce56fdd2bd759b704835056210e632e0d0
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-10 16:25:33 +0800

    node_01

--- NODE/code/01_learn-node/01_JavaScript代码的运行/index.js
+++ NODE/code/01_learn-node/01_JavaScript代码的运行/index.js
@@ -1,4 +1,19 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// console.log(sum(20, 30));
+// console.log(sum(20, 30));
+// console.log(sum(20, 30));
+// console.log(sum("aaa", "bbb"));
+
+// setTimeout(() => {
+//   console.log("定时器被执行");
+// }, 1000);
+
+console.log("Hello World")
 
 function sum(num1, num2) {
   return num1 + num2;
@@ -10,5 +25,5 @@ console.log(sum(20, 30));
 console.log(sum("aaa", "bbb"));
 
 setTimeout(() => {
-  console.log("定时器被执行");
-}, 1000);
+  console.log('定时器被执行');
+},1000)
