a1f076255ea455fd3a6f7a53c3cb5c03df70e24e
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-18 18:01:45 +0800

    7/18_18:00

--- 手写题与算法/4_数组去重.js
+++ 手写题与算法/4_数组去重.js
@@ -1,3 +1,10 @@
 function uniqueArr(arr) {
     return [...new Set(arr)];
 }
+// function unique(arr) {
+//     return [...new Set(arr)]
+// }
+
+// const MyArr = (arr) => {
+//     return [...new Set(arr)]
+// }
