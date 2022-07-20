ae1c733e6355a7bf39a511514709d6837e16c9d0
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-20 22:05:19 +0800

    手写题_01

---
+++ 手写题与算法/new.js
@@ -0,0 +1,23 @@
+// 8 new 操作符
+// 题目描述:手写 new 操作符实现
+// 实现代码如下:
+function myNew(fn, ...args) {
+  let obj = Object.create(fn.prototype);
+  let res = fn.call(obj, ...args);
+  if (res && (typeof res === "object" || typeof res === "function")) {
+    return res;
+  }
+  return obj;
+}
+// 用法如下：
+// // function Person(name, age) {
+// //   this.name = name;
+// //   this.age = age;
+// // }
+// // Person.prototype.say = function() {
+// //   console.log(this.age);
+// // };
+// // let p1 = myNew(Person, "lihua", 18);
+// // console.log(p1.name);
+// // console.log(p1);
+// // p1.say();
---
+++ 手写题与算法/深拷贝.js
@@ -0,0 +1,32 @@
+// 10 深拷贝（考虑到复制 Symbol 类型）
+// 题目描述:手写 new 操作符实现
+// 实现代码如下:
+function isObject(val) {
+  return typeof val === "object" && val !== null;
+}
+
+function deepClone(obj, hash = new WeakMap()) {
+  if (!isObject(obj)) return obj;
+  if (hash.has(obj)) {
+    return hash.get(obj);
+  }
+  let target = Array.isArray(obj) ? [] : {};
+  hash.set(obj, target);
+  Reflect.ownKeys(obj).forEach((item) => {
+    if (isObject(obj[item])) {
+      target[item] = deepClone(obj[item], hash);
+    } else {
+      target[item] = obj[item];
+    }
+  });
+
+  return target;
+}
+
+// var obj1 = {
+// a:1,
+// b:{a:2}
+// };
+// var obj2 = deepClone(obj1);
+// console.log(obj1);
+ 
