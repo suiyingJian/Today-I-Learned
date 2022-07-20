8b0092305a73bad73bd9a5dac091cb8efe469cae
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-07-20 22:21:07 +0800

    7/20_03

---
+++ 高级JavaScript/15_ES6中定义类的方法/02.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+var p1 = new Person("why", 18)
+var p2 = new Person('Likl', 30)
+console.log(p1,p2)
