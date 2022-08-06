ca357e5f9ee3af22c1678c8fa754863e008bbc36
Guxiu <62164319+WNXG@users.noreply.github.com>
2022-08-06 12:54:50 +0800

    0806_01

---
+++ 手写题与算法/test.js
@@ -0,0 +1,54 @@
+console.log(1);
+
+setTimeout(()=>{
+    console.log(2);   
+setTimeout(()=>{
+    console.log(14);   
+    new Promise((resolve,reject)=>{
+    console.log(15);
+    resolve()
+}).then(res=>{
+    console.log(16); 
+})
+})
+    
+new Promise((resolve,reject)=>{
+    console.log(3);
+    resolve()
+}).then(res=>{
+    console.log(4); 
+})
+})
+
+new Promise((resolve,reject)=>{
+    resolve()
+}).then(res=>{
+    console.log(5); 
+}).then(res=>{
+    console.log(6);
+    
+})
+
+new Promise((resolve,reject)=>{
+    console.log(7);
+    resolve()
+}).then(res=>{
+    console.log(8); 
+}).then(res=>{
+    console.log(9);
+    
+})
+
+setTimeout(()=>{
+    console.log(10);   
+    new Promise((resolve,reject)=>{
+    console.log(11);
+    resolve()
+}).then(res=>{
+    console.log(12); 
+})
+})
+
+console.log(13); 
+
+// 1 7 13 5 8 6 9 2 3 4 10 11 12 14 15 16
